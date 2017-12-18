3bdffd1aafbb59e7db3aa1f846dacdb6b38882b5
Chai-NED <Csadvisor@outlook.com>
2017-12-17 19:00:32 -0500

    Created StopVM.ps1 in AzureAutomation.

---
+++ aaprofxRunbooks/StopVM.ps1
@@ -0,0 +1,10 @@
+Param(
+    [parameter(Mandatory=$True)]
+    [string] $vmName
+)
+
+$Conn = Get-AutomationConnection -Name AzureRunAsConnection
+Add-AzureRMAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationID $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
+
+$vm = Get-AzureRmResource | where {$_.ResourceType -like "Microsoft.*/virtualMachines" -and $_.Name -eq $vmName}
+Stop-AzureRmVM -name  $vm.Name -ResourceGroupName $vm.ResourceGroupName -force
